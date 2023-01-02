da69e81c5dd290a6e702358af4d8e4e8007fc060
Sheng Luo <luosheng1986@gmail.com>
2023-01-02 20:52:27 +0800

    Fix typo

--- Sources/MastodonKit/Models/Poll.swift
+++ Sources/MastodonKit/Models/Poll.swift
@@ -22,6 +22,6 @@ public struct Poll: Codable {
   public let votersCount: Int
   public let options: [Option]
   public let emojis: [Emoji]
-  public let votes: Bool?
+  public let voted: Bool?
   public let ownVotes: [Int]?
 }
