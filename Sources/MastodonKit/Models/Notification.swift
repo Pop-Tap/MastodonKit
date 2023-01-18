0dfda8a75f9736be90cc772e6b0fa3c4aab82337
Sheng Luo <luosheng1986@gmail.com>
2023-01-18 15:24:18 +0800

    Make notification identifierable

--- Sources/MastodonKit/Models/Notification.swift
+++ Sources/MastodonKit/Models/Notification.swift
@@ -8,15 +8,15 @@
 
 import Foundation
 
-public struct Notification: Codable, Hashable {
-    /// The notification ID.
-    public let id: String
-    /// The notification type.
-    public let type: NotificationType
-    /// The time the notification was created.
-    public let createdAt: Date
-    /// The Account sending the notification to the user.
-    public let account: Account
-    /// The Status associated with the notification, if applicable.
-    public let status: Status?
+public struct Notification: Codable, Hashable, Identifiable {
+  /// The notification ID.
+  public let id: String
+  /// The notification type.
+  public let type: NotificationType
+  /// The time the notification was created.
+  public let createdAt: Date
+  /// The Account sending the notification to the user.
+  public let account: Account
+  /// The Status associated with the notification, if applicable.
+  public let status: Status?
 }
