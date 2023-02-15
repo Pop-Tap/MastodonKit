6b187f6d774483609ed011cf431e238b438ea5e3
Sheng Luo <luosheng1986@gmail.com>
2023-02-16 00:05:11 +0800

    Fix url encoding

--- Sources/MastodonKit/Foundation/URLComponents.swift
+++ Sources/MastodonKit/Foundation/URLComponents.swift
@@ -9,17 +9,18 @@
 import Foundation
 
 extension URLComponents {
-    init?<A>(baseURL: String, request: Request<A>) {
-        guard
-            let realBaseURL = URL(string: baseURL),
-            let completeURL = URL(string: request.path, relativeTo: realBaseURL)
-            else {
-                return nil
-        }
-
-        self.init(url: completeURL, resolvingAgainstBaseURL: true)
-
-        path = request.path
-        queryItems = request.method.queryItems
+  init?<A>(baseURL: String, request: Request<A>) {
+    guard
+      let realBaseURL = URL(string: baseURL),
+      let encodedPath = request.path.addingPercentEncoding(withAllowedCharacters: .urlFragmentAllowed),
+      let completeURL = URL(string: encodedPath, relativeTo: realBaseURL)
+    else {
+      return nil
     }
+
+    self.init(url: completeURL, resolvingAgainstBaseURL: true)
+
+    path = request.path
+    queryItems = request.method.queryItems
+  }
 }
